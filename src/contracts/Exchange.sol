7b86fd5f4ce214152d363b368569e2ee71b0a4c3
Amir Nickroo <amiracledev@gmail.com>
2019-07-05 11:50:18 -0700

    making orders added for testing

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -4,15 +4,15 @@ import './Token.sol';
 //   TODO:
 //   [X] Set the fee Account
 //   [X] Deposit Ether
-//   [] Withdraw Ether
+//   [X] Withdraw Ether
 //   [X] Deposit Tokens
-//   [] Withdraw Tokens
-//   [] Check Balances
+//   [X] Withdraw Tokens
+//   [X] Check Balances
 //   [] Make Order 
 //   [] Cancel Order 
 //   [] Fill Order 
 //   [] Charge Fees
-//   [] add safeguard so randoms cant send ether to us ..bc they have no way to withdraw it - Fallback Safety
+//   [Xgit ] add safeguard so randoms cant send ether to us ..bc they have no way to withdraw it - Fallback Safety
 //   
 	contract Exchange {
 	using SafeMath for uint;
@@ -21,9 +21,36 @@ import './Token.sol';
 	address constant ETHER = address(0); //allows us to store Ether in tokens mapping with blank address
 	mapping(address => mapping(address => uint256)) public tokens;
 
+	mapping(uint256 => _Order) public orders;
+	uint256 public orderCount;
+
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
+	event Order(
+		uint id,
+		address user,
+		address tokenGet,
+		uint amountGet,
+		address tokenGive,
+		uint amountGive,
+		uint timestamp
+		);
+
+	struct _Order {
+		uint id;
+		address user; //person who made the order
+		address tokenGet; //address of the toekn the want
+		uint amountGet; // amount of tokens they want
+		address tokenGive; //token they are going to give
+		uint amountGive; //amount they are going to give
+		uint timestamp; 
+
+	}
+
+		//Model the order
+		//a way to store the order
+		//add the order to storage
 
 	constructor (address _feeAccount, uint256 _feePercent) public {
 	feeAccount = _feeAccount;
@@ -48,14 +75,14 @@ import './Token.sol';
 	}
 
 	function depositToken(address _token, uint _amount) public {
-		//make sure no ether tokend deposited...check to see if its ether address
-		require(_token != ETHER);
+		//make sure no ether token deposited...check to see if its ether address
+	require(_token != ETHER);
 	require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 	tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
 	emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
 }
 
-function withdrawToken(address _token, uint256 _amount) public {
+	function withdrawToken(address _token, uint256 _amount) public {
         require(_token != ETHER);
         require(tokens[_token][msg.sender] >= _amount);
         tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
@@ -65,5 +92,11 @@ function withdrawToken(address _token, uint256 _amount) public {
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    	orderCount = orderCount.add(1);
+    	orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    	emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
     }
 }
